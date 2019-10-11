ea93b620bde22e527f06045fec3f6320186f2b1b
martinohmann <martinohmann@gmail.com>
2019-10-11 15:24:07 +0200

    add unittests for site24x7_action resource (#4)

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Bonial-International-GmbH/terraform-provider-site24x7
 go 1.12
 
 require (
-	github.com/Bonial-International-GmbH/site24x7-go v0.0.0-20191010114601-2e6a1e4efcfd
+	github.com/Bonial-International-GmbH/site24x7-go v0.0.0-20191011120600-568ee03b5b42
 	github.com/hashicorp/terraform-plugin-sdk v1.1.1
+	github.com/stretchr/testify v1.3.0
 )
---
+++ site24x7/action_test.go
@@ -0,0 +1,140 @@
+package site24x7
+
+import (
+	"testing"
+
+	"github.com/Bonial-International-GmbH/site24x7-go/api"
+	apierrors "github.com/Bonial-International-GmbH/site24x7-go/api/errors"
+	"github.com/Bonial-International-GmbH/site24x7-go/fake"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestActionCreate(t *testing.T) {
+	d := actionTestResourceData(t)
+
+	c := fake.NewClient()
+
+	a := &api.ITAutomation{
+		ActionName:             "foobar",
+		ActionMethod:           "P",
+		CustomParameters:       "foobarbaz",
+		SendCustomParameters:   true,
+		SendInJsonFormat:       true,
+		SendIncidentParameters: false,
+		ActionTimeout:          30,
+		ActionUrl:              "https://example.com",
+		ActionType:             1,
+	}
+
+	c.FakeITAutomations.On("Create", a).Return(a, nil).Once()
+
+	require.NoError(t, actionCreate(d, c))
+
+	c.FakeITAutomations.On("Create", a).Return(a, apierrors.NewStatusError(500, "error")).Once()
+
+	err := actionCreate(d, c)
+
+	assert.Equal(t, apierrors.NewStatusError(500, "error"), err)
+}
+
+func TestActionUpdate(t *testing.T) {
+	d := actionTestResourceData(t)
+	d.SetId("123")
+
+	c := fake.NewClient()
+
+	a := &api.ITAutomation{
+		ActionID:               "123",
+		ActionName:             "foobar",
+		ActionMethod:           "P",
+		CustomParameters:       "foobarbaz",
+		SendCustomParameters:   true,
+		SendInJsonFormat:       true,
+		SendIncidentParameters: false,
+		ActionTimeout:          30,
+		ActionUrl:              "https://example.com",
+		ActionType:             1,
+	}
+
+	c.FakeITAutomations.On("Update", a).Return(a, nil).Once()
+
+	require.NoError(t, actionUpdate(d, c))
+
+	c.FakeITAutomations.On("Update", a).Return(a, apierrors.NewStatusError(500, "error")).Once()
+
+	err := actionUpdate(d, c)
+
+	assert.Equal(t, apierrors.NewStatusError(500, "error"), err)
+}
+
+func TestActionRead(t *testing.T) {
+	d := actionTestResourceData(t)
+	d.SetId("123")
+
+	c := fake.NewClient()
+
+	c.FakeITAutomations.On("Get", "123").Return(&api.ITAutomation{}, nil).Once()
+
+	require.NoError(t, actionRead(d, c))
+
+	c.FakeITAutomations.On("Get", "123").Return(nil, apierrors.NewStatusError(500, "error")).Once()
+
+	err := actionRead(d, c)
+
+	assert.Equal(t, apierrors.NewStatusError(500, "error"), err)
+}
+
+func TestActionDelete(t *testing.T) {
+	d := actionTestResourceData(t)
+	d.SetId("123")
+
+	c := fake.NewClient()
+
+	c.FakeITAutomations.On("Delete", "123").Return(nil).Once()
+
+	require.NoError(t, actionDelete(d, c))
+}
+
+func TestActionExists(t *testing.T) {
+	d := actionTestResourceData(t)
+	d.SetId("123")
+
+	c := fake.NewClient()
+
+	c.FakeITAutomations.On("Get", "123").Return(&api.ITAutomation{}, nil).Once()
+
+	exists, err := actionExists(d, c)
+
+	require.NoError(t, err)
+	assert.True(t, exists)
+
+	c.FakeITAutomations.On("Get", "123").Return(nil, apierrors.NewStatusError(404, "not found")).Once()
+
+	exists, err = actionExists(d, c)
+
+	require.NoError(t, err)
+	assert.False(t, exists)
+
+	c.FakeITAutomations.On("Get", "123").Return(nil, apierrors.NewStatusError(500, "error")).Once()
+
+	exists, err = actionExists(d, c)
+
+	require.Equal(t, apierrors.NewStatusError(500, "error"), err)
+	assert.False(t, exists)
+}
+
+func actionTestResourceData(t *testing.T) *schema.ResourceData {
+	return schema.TestResourceDataRaw(t, ActionSchema, map[string]interface{}{
+		"name":                     "foobar",
+		"method":                   "P",
+		"custom_parameters":        "foobarbaz",
+		"send_custom_parameters":   true,
+		"send_in_json_format":      true,
+		"send_incident_parameters": false,
+		"timeout":                  30,
+		"url":                      "https://example.com",
+		"type":                     1,
+	})
+}
