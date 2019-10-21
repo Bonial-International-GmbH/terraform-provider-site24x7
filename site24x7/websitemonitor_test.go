ce2d242bd906d5994e70d6cf023dca3519a0869b
martinohmann <martinohmann@gmail.com>
2019-10-21 13:33:42 +0200

    add more tests (#8)

--- site24x7/websitemonitor_test.go
+++ site24x7/websitemonitor_test.go
@@ -1,6 +1,7 @@
 package site24x7
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/Bonial-International-GmbH/site24x7-go/api"
@@ -8,131 +9,268 @@ import (
 	"github.com/Bonial-International-GmbH/site24x7-go/fake"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
 func TestWebsiteMonitorCreate(t *testing.T) {
-	d := monitorTestResourceData(t)
+	tests := []struct {
+		name                 string
+		setup                func(t *testing.T, c *fake.Client)
+		resourceDataProvider func(t *testing.T) *schema.ResourceData
+		expectedErr          error
+		validate             func(t *testing.T, c *fake.Client)
+	}{
+		{
+			name: "create simple monitor",
+			setup: func(t *testing.T, c *fake.Client) {
+				a := &api.Monitor{
+					DisplayName:           "foo",
+					Type:                  "URL",
+					Website:               "www.test.tld",
+					CheckFrequency:        "1",
+					HTTPMethod:            "G",
+					Timeout:               10,
+					LocationProfileID:     "456",
+					NotificationProfileID: "789",
+					ThresholdProfileID:    "012",
+					UseNameServer:         true,
+					UserGroupIDs:          []string{"123"},
+				}
 
-	c := fake.NewClient()
-
-	a := &api.Monitor{
-		DisplayName:    "foo",
-		Type:           "URL",
-		Website:        "www.test.tld",
-		CheckFrequency: "60",
-		HTTPMethod:     "P",
-		AuthUser:       "username",
-		AuthPass:       "password",
-		MatchCase:      true,
-		UserAgent:      "firefox",
-		CustomHeaders: []api.Header{
-			{
-				Name:  "Header Name",
-				Value: "testheader",
+				c.FakeMonitors.On("Create", a).Return(a, nil).Once()
 			},
-			{
-				Name:  "cache",
-				Value: "nocache",
+			resourceDataProvider: func(t *testing.T) *schema.ResourceData {
+				return schema.TestResourceDataRaw(t, WebsiteMonitorSchema, map[string]interface{}{
+					"display_name":            "foo",
+					"type":                    "URL",
+					"website":                 "www.test.tld",
+					"location_profile_id":     "456",
+					"notification_profile_id": "789",
+					"threshold_profile_id":    "012",
+					"user_group_ids":          []interface{}{"123"},
+				})
 			},
 		},
-		Timeout:               120,
-		LocationProfileID:     "456",
-		NotificationProfileID: "789",
-		ThresholdProfileID:    "012",
-		MonitorGroups: []string{
-			"234",
-			"567",
-		},
-		UserGroupIDs: []string{
-			"123",
-			"456",
-		},
-		UseNameServer: true,
-		ActionIDs: []api.ActionRef{
-			{
-				ActionID:  "123action",
-				AlertType: 1,
+		{
+			name: "passes through create monitor error",
+			setup: func(t *testing.T, c *fake.Client) {
+				c.FakeMonitors.On("Create", mock.Anything).Return(nil, apierrors.NewStatusError(500, "server error")).Once()
 			},
-			{
-				ActionID:  "234action",
-				AlertType: 5,
+			resourceDataProvider: func(t *testing.T) *schema.ResourceData {
+				return schema.TestResourceDataRaw(t, WebsiteMonitorSchema, map[string]interface{}{
+					"display_name":            "foo",
+					"type":                    "URL",
+					"website":                 "www.test.tld",
+					"location_profile_id":     "456",
+					"notification_profile_id": "789",
+					"threshold_profile_id":    "012",
+					"user_group_ids":          []interface{}{"123"},
+				})
 			},
+			expectedErr: apierrors.NewStatusError(500, "server error"),
+		},
+		{
+			name: "somebody tampered with the statefile",
+			resourceDataProvider: func(t *testing.T) *schema.ResourceData {
+				return schema.TestResourceDataRaw(t, WebsiteMonitorSchema, map[string]interface{}{
+					"display_name":            "foo",
+					"type":                    "URL",
+					"website":                 "www.test.tld",
+					"location_profile_id":     "456",
+					"notification_profile_id": "789",
+					"threshold_profile_id":    "012",
+					"actions": map[string]interface{}{
+						"this-will-cause-an-error": "123action",
+					},
+					"user_group_ids": []interface{}{"123"},
+				})
+			},
+			validate: func(t *testing.T, c *fake.Client) {
+				assert.Len(t, c.FakeMonitors.Calls, 0)
+			},
+			expectedErr: errors.New(`strconv.Atoi: parsing "this-will-cause-an-error": invalid syntax`),
 		},
 	}
 
-	c.FakeMonitors.On("Create", a).Return(a, nil).Once()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			c := fake.NewClient()
+			d := test.resourceDataProvider(t)
 
-	require.NoError(t, websiteMonitorCreate(d, c))
+			if test.setup != nil {
+				test.setup(t, c)
+			}
 
-	c.FakeMonitors.On("Create", a).Return(a, apierrors.NewStatusError(500, "error")).Once()
+			err := websiteMonitorCreate(d, c)
+			if test.expectedErr != nil {
+				require.Error(t, err)
+				assert.Equal(t, test.expectedErr.Error(), err.Error())
+			} else {
+				require.NoError(t, err)
+			}
 
-	err := websiteMonitorCreate(d, c)
-
-	assert.Equal(t, apierrors.NewStatusError(500, "error"), err)
+			if test.validate != nil {
+				test.validate(t, c)
+			}
+		})
+	}
 }
 
 func TestWebsiteMonitorUpdate(t *testing.T) {
-	d := monitorTestResourceData(t)
-	d.SetId("123")
+	tests := []struct {
+		name                 string
+		setup                func(t *testing.T, c *fake.Client)
+		resourceDataProvider func(t *testing.T) *schema.ResourceData
+		expectedErr          error
+		validate             func(t *testing.T, c *fake.Client)
+	}{
+		{
+			name: "updates simple monitor",
+			setup: func(t *testing.T, c *fake.Client) {
+				a := &api.Monitor{
+					MonitorID:             "123",
+					DisplayName:           "foo",
+					Type:                  "URL",
+					Website:               "www.test.tld",
+					CheckFrequency:        "1",
+					HTTPMethod:            "G",
+					Timeout:               10,
+					LocationProfileID:     "456",
+					NotificationProfileID: "789",
+					ThresholdProfileID:    "012",
+					UseNameServer:         true,
+					UserGroupIDs:          []string{"123"},
+					CustomHeaders: []api.Header{
+						{
+							Name:  "Accept",
+							Value: "application/json",
+						},
+						{
+							Name:  "Cache-Control",
+							Value: "nocache",
+						},
+					},
+					ActionIDs: []api.ActionRef{
+						{
+							ActionID:  "123action",
+							AlertType: 1,
+						},
+						{
+							ActionID:  "234action",
+							AlertType: 5,
+						},
+					},
+					UnmatchingKeyword: &api.ValueAndSeverity{
+						Value:    "foo",
+						Severity: 2,
+					},
+					MatchingKeyword: &api.ValueAndSeverity{
+						Value:    "bar",
+						Severity: 2,
+					},
+					MatchRegex: &api.ValueAndSeverity{
+						Value:    ".*",
+						Severity: 2,
+					},
+				}
 
-	c := fake.NewClient()
+				c.FakeMonitors.On("Update", a).Return(a, nil).Once()
+			},
+			resourceDataProvider: func(t *testing.T) *schema.ResourceData {
+				rd := schema.TestResourceDataRaw(t, WebsiteMonitorSchema, map[string]interface{}{
+					"display_name":            "foo",
+					"type":                    "URL",
+					"website":                 "www.test.tld",
+					"location_profile_id":     "456",
+					"notification_profile_id": "789",
+					"threshold_profile_id":    "012",
+					"user_group_ids":          []interface{}{"123"},
+					"custom_headers": map[string]interface{}{
+						"Accept":        "application/json",
+						"Cache-Control": "nocache",
+					},
+					"actions": map[string]interface{}{
+						"1": "123action",
+						"5": "234action",
+					},
+					"unmatching_keyword_value": "foo",
+					"matching_keyword_value":   "bar",
+					"match_regex_value":        ".*",
+				})
 
-	a := &api.Monitor{
-		MonitorID:      "123",
-		DisplayName:    "foo",
-		Type:           "URL",
-		Website:        "www.test.tld",
-		CheckFrequency: "60",
-		HTTPMethod:     "P",
-		AuthUser:       "username",
-		AuthPass:       "password",
-		MatchCase:      true,
-		UserAgent:      "firefox",
-		CustomHeaders: []api.Header{
-			{
-				Name:  "Header Name",
-				Value: "testheader",
-			},
-			{
-				Name:  "cache",
-				Value: "nocache",
+				rd.SetId("123")
+
+				return rd
 			},
 		},
-		Timeout:               120,
-		LocationProfileID:     "456",
-		NotificationProfileID: "789",
-		ThresholdProfileID:    "012",
-		MonitorGroups: []string{
-			"234",
-			"567",
-		},
-		UserGroupIDs: []string{
-			"123",
-			"456",
-		},
-		UseNameServer: true,
-		ActionIDs: []api.ActionRef{
-			{
-				ActionID:  "123action",
-				AlertType: 1,
+		{
+			name: "passes through create monitor error",
+			setup: func(t *testing.T, c *fake.Client) {
+				c.FakeMonitors.On("Update", mock.Anything).Return(nil, apierrors.NewStatusError(500, "server error")).Once()
 			},
-			{
-				ActionID:  "234action",
-				AlertType: 5,
+			resourceDataProvider: func(t *testing.T) *schema.ResourceData {
+				rd := schema.TestResourceDataRaw(t, WebsiteMonitorSchema, map[string]interface{}{
+					"display_name":            "foo",
+					"type":                    "URL",
+					"website":                 "www.test.tld",
+					"location_profile_id":     "456",
+					"notification_profile_id": "789",
+					"threshold_profile_id":    "012",
+					"user_group_ids":          []interface{}{"123"},
+				})
+
+				rd.SetId("123")
+
+				return rd
 			},
+			expectedErr: apierrors.NewStatusError(500, "server error"),
+		},
+		{
+			name: "somebody tampered with the statefile",
+			resourceDataProvider: func(t *testing.T) *schema.ResourceData {
+				return schema.TestResourceDataRaw(t, WebsiteMonitorSchema, map[string]interface{}{
+					"display_name":            "foo",
+					"type":                    "URL",
+					"website":                 "www.test.tld",
+					"location_profile_id":     "456",
+					"notification_profile_id": "789",
+					"threshold_profile_id":    "012",
+					"actions": map[string]interface{}{
+						"this-will-cause-an-error": "123action",
+					},
+					"user_group_ids": []interface{}{"123"},
+				})
+			},
+			validate: func(t *testing.T, c *fake.Client) {
+				assert.Len(t, c.FakeMonitors.Calls, 0)
+			},
+			expectedErr: errors.New(`strconv.Atoi: parsing "this-will-cause-an-error": invalid syntax`),
 		},
 	}
 
-	c.FakeMonitors.On("Update", a).Return(a, nil).Once()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			c := fake.NewClient()
+			d := test.resourceDataProvider(t)
 
-	require.NoError(t, websiteMonitorUpdate(d, c))
+			if test.setup != nil {
+				test.setup(t, c)
+			}
 
-	c.FakeMonitors.On("Update", a).Return(a, apierrors.NewStatusError(500, "error")).Once()
+			err := websiteMonitorUpdate(d, c)
+			if test.expectedErr != nil {
+				require.Error(t, err)
+				assert.Equal(t, test.expectedErr.Error(), err.Error())
+			} else {
+				require.NoError(t, err)
+			}
 
-	err := websiteMonitorUpdate(d, c)
-
-	assert.Equal(t, apierrors.NewStatusError(500, "error"), err)
+			if test.validate != nil {
+				test.validate(t, c)
+			}
+		})
+	}
 }
 
 func TestWebsiteMonitorRead(t *testing.T) {
@@ -224,4 +362,203 @@ func monitorTestResourceData(t *testing.T) *schema.ResourceData {
 			"5": "234action",
 		},
 	})
+}
+
+func TestResourceDataToWebsiteMonitor(t *testing.T) {
+	tests := []struct {
+		name                 string
+		setup                func(t *testing.T, c *fake.Client)
+		resourceDataProvider func(t *testing.T) *schema.ResourceData
+		expected             *api.Monitor
+		expectedErr          error
+	}{
+		{
+			name: "simple",
+			resourceDataProvider: func(t *testing.T) *schema.ResourceData {
+				return schema.TestResourceDataRaw(t, WebsiteMonitorSchema, map[string]interface{}{
+					"display_name":            "foo",
+					"type":                    "URL",
+					"website":                 "www.test.tld",
+					"location_profile_id":     "456",
+					"notification_profile_id": "789",
+					"threshold_profile_id":    "012",
+					"user_group_ids":          []interface{}{"123"},
+				})
+			},
+			expected: &api.Monitor{
+				DisplayName:           "foo",
+				Type:                  "URL",
+				Website:               "www.test.tld",
+				CheckFrequency:        "1",
+				HTTPMethod:            "G",
+				Timeout:               10,
+				LocationProfileID:     "456",
+				NotificationProfileID: "789",
+				ThresholdProfileID:    "012",
+				UseNameServer:         true,
+				UserGroupIDs:          []string{"123"},
+			},
+		},
+		{
+			name: "fetches default location profile if not set",
+			resourceDataProvider: func(t *testing.T) *schema.ResourceData {
+				return schema.TestResourceDataRaw(t, WebsiteMonitorSchema, map[string]interface{}{
+					"display_name":            "foo",
+					"type":                    "URL",
+					"website":                 "www.test.tld",
+					"notification_profile_id": "789",
+					"threshold_profile_id":    "012",
+					"user_group_ids":          []interface{}{"123"},
+				})
+			},
+			setup: func(t *testing.T, c *fake.Client) {
+				c.FakeLocationProfiles.On("List").Return([]*api.LocationProfile{
+					{ProfileID: "345"},
+				}, nil)
+			},
+			expected: &api.Monitor{
+				DisplayName:           "foo",
+				Type:                  "URL",
+				Website:               "www.test.tld",
+				CheckFrequency:        "1",
+				HTTPMethod:            "G",
+				Timeout:               10,
+				LocationProfileID:     "345",
+				NotificationProfileID: "789",
+				ThresholdProfileID:    "012",
+				UseNameServer:         true,
+				UserGroupIDs:          []string{"123"},
+			},
+		},
+		{
+			name: "returns error if lookup of default location profile fails",
+			resourceDataProvider: func(t *testing.T) *schema.ResourceData {
+				return schema.TestResourceDataRaw(t, WebsiteMonitorSchema, map[string]interface{}{
+					"display_name":            "foo",
+					"type":                    "URL",
+					"website":                 "www.test.tld",
+					"notification_profile_id": "789",
+					"threshold_profile_id":    "012",
+					"user_group_ids":          []interface{}{"123"},
+				})
+			},
+			setup: func(t *testing.T, c *fake.Client) {
+				c.FakeLocationProfiles.On("List").Return(nil, apierrors.NewStatusError(503, "service unavailable"))
+			},
+			expectedErr: apierrors.NewStatusError(503, "service unavailable"),
+		},
+		{
+			name: "fetches default notification profile if not set",
+			resourceDataProvider: func(t *testing.T) *schema.ResourceData {
+				return schema.TestResourceDataRaw(t, WebsiteMonitorSchema, map[string]interface{}{
+					"display_name":         "foo",
+					"type":                 "URL",
+					"website":              "www.test.tld",
+					"location_profile_id":  "789",
+					"threshold_profile_id": "012",
+					"user_group_ids":       []interface{}{"123"},
+				})
+			},
+			setup: func(t *testing.T, c *fake.Client) {
+				c.FakeNotificationProfiles.On("List").Return([]*api.NotificationProfile{
+					{ProfileID: "345"},
+				}, nil)
+			},
+			expected: &api.Monitor{
+				DisplayName:           "foo",
+				Type:                  "URL",
+				Website:               "www.test.tld",
+				CheckFrequency:        "1",
+				HTTPMethod:            "G",
+				Timeout:               10,
+				LocationProfileID:     "789",
+				NotificationProfileID: "345",
+				ThresholdProfileID:    "012",
+				UseNameServer:         true,
+				UserGroupIDs:          []string{"123"},
+			},
+		},
+		{
+			name: "fetches default threshold profile if not set",
+			resourceDataProvider: func(t *testing.T) *schema.ResourceData {
+				return schema.TestResourceDataRaw(t, WebsiteMonitorSchema, map[string]interface{}{
+					"display_name":            "foo",
+					"type":                    "URL",
+					"website":                 "www.test.tld",
+					"location_profile_id":     "789",
+					"notification_profile_id": "012",
+					"user_group_ids":          []interface{}{"123"},
+				})
+			},
+			setup: func(t *testing.T, c *fake.Client) {
+				c.FakeThresholdProfiles.On("List").Return([]*api.ThresholdProfile{
+					{ProfileID: "345"},
+				}, nil)
+			},
+			expected: &api.Monitor{
+				DisplayName:           "foo",
+				Type:                  "URL",
+				Website:               "www.test.tld",
+				CheckFrequency:        "1",
+				HTTPMethod:            "G",
+				Timeout:               10,
+				LocationProfileID:     "789",
+				NotificationProfileID: "012",
+				ThresholdProfileID:    "345",
+				UseNameServer:         true,
+				UserGroupIDs:          []string{"123"},
+			},
+		},
+		{
+			name: "fetches default threshold profile if not set",
+			resourceDataProvider: func(t *testing.T) *schema.ResourceData {
+				return schema.TestResourceDataRaw(t, WebsiteMonitorSchema, map[string]interface{}{
+					"display_name":            "foo",
+					"type":                    "URL",
+					"website":                 "www.test.tld",
+					"location_profile_id":     "789",
+					"notification_profile_id": "012",
+					"threshold_profile_id":    "345",
+				})
+			},
+			setup: func(t *testing.T, c *fake.Client) {
+				c.FakeUserGroups.On("List").Return([]*api.UserGroup{
+					{UserGroupID: "345"},
+				}, nil)
+			},
+			expected: &api.Monitor{
+				DisplayName:           "foo",
+				Type:                  "URL",
+				Website:               "www.test.tld",
+				CheckFrequency:        "1",
+				HTTPMethod:            "G",
+				Timeout:               10,
+				LocationProfileID:     "789",
+				NotificationProfileID: "012",
+				ThresholdProfileID:    "345",
+				UseNameServer:         true,
+				UserGroupIDs:          []string{"345"},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			c := fake.NewClient()
+			d := test.resourceDataProvider(t)
+
+			if test.setup != nil {
+				test.setup(t, c)
+			}
+
+			monitor, err := resourceDataToWebsiteMonitor(d, c)
+			if test.expectedErr != nil {
+				require.Error(t, err)
+				assert.Equal(t, test.expectedErr.Error(), err.Error())
+			} else {
+				require.NoError(t, err)
+				assert.Equal(t, test.expected, monitor)
+			}
+		})
+	}
 }
